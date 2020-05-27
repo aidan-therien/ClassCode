01bb6a15827fb2153d83ba32fbb97080e85f331e
Aidan <aidan.therien@duke.edu>
2020-05-27 10:33:42 -0400

    added files

---
+++ Caclulator.py
@@ -0,0 +1,31 @@
+def calc_input():		
+	a = input("input your first number: ")
+	b = input("input your second number: ")
+	a = int(a);
+	b = int(b);
+	print("The numbers your entered are {} and {}".format(a,b))
+
+def add()
+	print("Add")
+	answer = a + b
+	print ("{} + {} = {}".format(a,b,answer))
+
+def subtract()
+	print("subtract")
+	answer = a - b
+	print("{} -w {} = {}".format(a,b,answer))
+
+def multiply()
+	print("multiply")
+	answer = a*b
+	print("{} * {} = {}".format(a,b,answer))
+def math_command()
+	c = input("Enter a command: ")
+	if c == 'a':
+		add()
+	elif c == 's'
+		subtract()
+
+calc_input()
+math_command()
+print("finished")
---
+++ test.py
@@ -0,0 +1,4 @@
+
+
+def test_calculator():
+	from calculator
